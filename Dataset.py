40c44db8e24229d2958dba21701f98d125873500
Ali Kozlu <akozlu1@gmail.com>
2018-06-16 15:15:13 +0300

    Implemented common opponent method

--- Dataset.py
+++ Dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time
 
 
 class Dataset(object):
@@ -13,39 +14,51 @@ class Dataset(object):
         self.X = []
         self.y = []
 
-    def find_common_opponents(self):
+    def print_results(self):
+
+        print(self.store['stats'].head(20))
+
+    def create_feature_set(self):
         stats = self.store['stats']  # uncomment this line
         common_more_than_10 = 0
-        for i in stats.index:
-            opponents_of_p1 = []
-            opponents_of_p2 = []
+        start_time = time.time()
+
+        for i in reversed(stats.index):
+
+            print(i)
             player1_id = stats.at[i, "ID1"]
             player1_games = stats.loc[np.logical_or(stats.ID1 == player1_id, stats.ID2 == player1_id)]
+
+            curr_tournament = float(stats.at[i, "ID_T"])
+
+            earlier_games_of_p1 = [game for index, game in player1_games.iterrows() if
+                                   (float(game['ID_T']) < curr_tournament)]
+
             player2_id = stats.at[i, "ID2"]
             player2_games = stats.loc[np.logical_or(stats.ID1 == player2_id, stats.ID2 == player2_id)]
 
-            for j in player1_games.index:
-                if player1_id == player1_games.at[j, "ID1"]:
-                    opponents_of_p1.append(player1_games.at[j, "ID2"])
-                else:
-                    opponents_of_p1.append(player1_games.at[j, "ID1"])
+            earlier_games_of_p2 = [game for index, game in player2_games.iterrows() if
+                                   (float(game['ID_T']) < curr_tournament)]
 
-            for k in player2_games.index:
-                if player2_id == player2_games.at[k, "ID1"]:
-                    opponents_of_p2.append(player2_games.at[k, "ID2"])
-                else:
-                    opponents_of_p2.append(player2_games.at[k, "ID1"])
+            opponents_of_p1 = [
+                games['ID2'] if (player1_id == games['ID1']) else
+                games['ID1'] for games in earlier_games_of_p1]
+
+            opponents_of_p2 = [
+                games['ID2'] if (player1_id == games['ID1']) else
+                games['ID1'] for games in earlier_games_of_p2]
 
             sa = set(opponents_of_p1)
             sb = set(opponents_of_p2)
             c = sa.intersection(sb)
+
             if (len(c) > 10):
                 common_more_than_10 = common_more_than_10 + 1
 
         print(common_more_than_10)
-            #s1 = pd.merge(dfA, dfB, how='inner', on=['S', 'T']) # you can do this by comparing pandadataframes where two players have ID1 and you want to get same ID2 players
-
+        print("Time took for creating stat features for each match took--- %s seconds ---" % (time.time() - start_time))
 
 
 DT = Dataset("db.sqlite")
-DT.find_common_opponents()
+# DT.create_feature_set()
+DT.create_feature_set()
